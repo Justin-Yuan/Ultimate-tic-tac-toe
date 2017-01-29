3f0face6bc7921b13afb951a3fcb4110d176f01c
Ted Huang <Ted Huang>
2017-01-28 23:21:49 -0500

    adding helper functions

--- program19.py
+++ program19.py
@@ -1,3 +1,10 @@
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+
+global winScores		#how much we are winning each board by (9 element array) from 0 to 1 (0.5 = tie)
+global prScores			#how much we want to win each board (9 element array) from 0 to 1 
+
 class square:
 	def __init__(self,ID,val):
 		self.ID = ID
@@ -12,16 +19,15 @@ class square:
 
 def findValidMoves(squares,nextsquare):
 	vm = []
-	for i in range(81):
-		if squares[i].bigSq==nextsquare or nextsquare>8:  #Have to play in the next square, unless you can play anywhere
-			if squares[i].value == 0: #Square must be empty
-				if isBoardWon(getBigBoard(squares,squares[i].bigSq))==0: #Can't play in a won board
-					if not isBoardFull(getBigBoard(squares,squares[i].bigSq)): #Can't play in a full board
-						vm.append(i)
+	for i in range(nextsquare*9, nextsquare*10):
+		if squares[i].value == 0: #Square must be empty
+		vm.append(nextsquare*9 + i)
 	return vm
 
+for i in range(nextsquare*9,nextsquare*10): 
+				squares.append(square(i-2,data[i]))
 
-def isBoardWon(squares):
+def boardWinner(squares):
 	#Input: squares = 8 item list of squares 
 	#Output: 0 if not win, 1 if 1 won, 2 if 2 won
 	def compareSquares(squares,s1,s2,s3,v):
@@ -53,10 +59,61 @@ def isBoardFull(squares):
 			return False
 	return True
 
+def isBoardEmpty(squares):
+	for i in range(9):
+		if squares[i]!=0:
+			return False
+	return True
+
 def getBigBoard(squares,bigSq):
 	sq = []
 	for i in range(81):
 		if squares[i].bigSq == bigSq:
 			sq.append(squares[i].value)
 	return sq
+
+def winScore(board, player):
+	if boardWinner(board)==player:
+		return 1;
+	if boardWinner(board)!= 0
+		return 0;
+	if isBoardFull(board).............
+
+def updateScores(game, player):
+	for i in range (0, 8):
+		if winScores[i]!=0 and winScores[i]!=1:
+			winScores[i] = winScore(game[i*9, i*10])
+	
+
+def get_move(timeout, data):
+
+	game = data([2:83]
+
+	if (time < changeAlgTime):
+
+		PLAYER=data[0]
+		nextsquare=data[1]
+
+		#if going first
+		if gameJustStarted():
+			return 40
+
+
+		if nextsquare != 9:
+
+			#getting valid moves
+			for i in range(nextsquare*9,nextsquare*10): 
+				squares.append(square(i-2,data[i]))
+			vm = findValidMoves(squares, nextsquare)
+
+
+			#cheesing
+
+
+
+		else:
+
+	else:
+
+
 
